16a6bbd20eaab63a2c4bc3d72ff6cb37bc4aeced
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-06-10 16:37:53 +0530

    fixed images

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Modal.css"
+import Rules from "../Components/images/image-rules.svg"
+import RulesBonus from "../Components/images/image-rules-bonus.svg"
+import Iconclose from "../Components/images/icon-close.svg"
 export default function Modal({ mode, modalClass, SetModalClass }) {
     
     return (
@@ -7,9 +10,9 @@ export default function Modal({ mode, modalClass, SetModalClass }) {
             <div className="overlay"></div>
             <div className="modal-container">
                 <p>RULES</p>
-                <img src = {mode==='basic'?'./images/image-rules.svg':'./images/image-rules-bonus.svg'} alt = "modal-bg"/>
+                <img src = {mode==='basic'?Rules:RulesBonus} alt = "modal-bg"/>
                 <div className="icon-close" onClick={()=>{SetModalClass('modal')}}>
-                    <img src = "../images/icon-close.svg" alt = "icon-close" />
+                    <img src = {Iconclose} alt = "icon-close" />
                 </div>
             </div>
         </section>
