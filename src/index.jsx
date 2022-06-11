5aae2b28328dfdf601079f16b96cf27afe31bd60
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-06-11 10:17:37 +0530

    modified

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM, { createRoot } from "react-dom/client"
 import App from "./App"
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
